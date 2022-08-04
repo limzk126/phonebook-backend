05b73a3d6eaea2d5b95da39b199e5e9333fea340
puzzlehead <dylanlim12698@gmail.com>
2022-08-04 17:37:12 +0800

    Upload 3.6

--- index.js
+++ index.js
@@ -83,6 +83,12 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
+  if (persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())) {
+    return response.status(400).json({
+        error: "name must be unique",
+    })
+  }
+
   const newPerson = {
     id: generateId(),
     name: body.name,
